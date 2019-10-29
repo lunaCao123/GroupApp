457134c65d6c18afea72d87dbf9d8d3e68999ba2
Luna Cao <z5045863@student.unsw.edu.au>
2019-10-29 15:05:32 +1100

    homePageDone

--- BarBootCamp1/app/src/main/java/com/example/barbootcamp/MainActivity.java
+++ BarBootCamp1/app/src/main/java/com/example/barbootcamp/MainActivity.java
@@ -2,13 +2,37 @@ package com.example.barbootcamp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    public ImageButton startButton;
+    public ImageView logoImage;
+    public EditText userNameEditText;
+    public static String userName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        logoImage = findViewById(R.id.welcomPage_logo);
+        logoImage.setImageResource(R.drawable.ic_cocktail_glass_with_a_orange_slice_svgrepo_com);
+
+        userNameEditText = findViewById(R.id.welcomePage_userNameEditText);
+        userName = userNameEditText.getText().toString();
+
+        startButton = findViewById(R.id.welcomePage_startButton);
+        startButton.setImageResource(R.drawable.ic_arrow_forward_gold);
+
+    }
+    public void setStartButton(View view){
+        Intent intent = new Intent(this, ChooseUserTypeActivity.class);
+        intent.putExtra("userName", userName);
+        startActivity(intent);
     }
 }
